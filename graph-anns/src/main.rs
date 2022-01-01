5a2b1254fe058c1d23a52d6f16f02158e31744e2
Connor Clark <connor.r.clark@gmail.com>
2021-12-31 19:02:10 -0500

    TODO: investigate perf diff of as vs into
    The sq_euclidean_distance using into is much, much slower
    than the version using as. Ask on SO if you can't find why.
    
    This might be related: https://stdrs.dev/nightly/x86_64-pc-windows-gnu/src/core/convert/num.rs.html#45-69

--- graph-anns/src/main.rs
+++ graph-anns/src/main.rs
@@ -1,15 +1,19 @@
 #![feature(total_cmp)]
 extern crate memmap;
+extern crate nix;
+use nix::libc::c_void;
+use nix::sys::mman::{mmap, MapFlags, ProtFlags};
 use std::cmp::Ordering;
 use std::collections::binary_heap::BinaryHeap;
 use std::convert::TryInto;
 use std::fs::File;
+use std::os::unix::io::IntoRawFd;
 
 // A handle to a file in the bvecs_array, ivecs_array, or fvecs_array format on
 // disk. The file is mmaped. The associated functions are used to read vectors
 // from the file.
 struct Vecs<'a, T> {
-  _mmap: memmap::Mmap, // segfaults unless we carry this around.
+  _mmap: *mut c_void, // segfaults unless we carry this around.
   pub num_rows: usize,
   pub num_dim: usize,
   buffer: &'a [T],
@@ -28,9 +32,19 @@ impl<'a, T> Vecs<'_, T> {
       .try_into()
       .unwrap();
     let num_rows = filesize / (num_dim * std::mem::size_of::<T>());
-    let _mmap = unsafe { memmap::MmapOptions::new().map(&f) }.expect("mmap failed");
-    let buffer =
-      unsafe { std::slice::from_raw_parts(_mmap.as_ptr() as *const T, num_rows * num_dim) };
+    //let _mmap = unsafe { memmap::MmapOptions::new().map(&f) }.expect("mmap failed");
+    let _mmap = unsafe {
+      mmap(
+        std::ptr::null_mut(),
+        filesize,
+        ProtFlags::PROT_READ,
+        MapFlags::MAP_PRIVATE | MapFlags::MAP_POPULATE,
+        f.into_raw_fd(),
+        0,
+      )
+      .expect("mmap failed")
+    };
+    let buffer = unsafe { std::slice::from_raw_parts(_mmap as *const T, num_rows * num_dim) };
     Ok(Self {
       _mmap,
       num_rows,
@@ -101,19 +115,82 @@ fn search_range<'a, T>(
   nearest_neighbors
 }
 
-fn sq_euclidean<T: Into<f32> + Copy>(v1: &[T], v2: &[T]) -> f32 {
+/*
+fn mainold() {
+  let base_vecs = Vecs::<f32>::new(
+    "/mnt/970pro/anns/siftsmall/siftsmall_learn.fvecs_array",
+    128,
+  )
+  .unwrap();
+  let query_vecs = Vecs::<f32>::new(
+    "/mnt/970pro/anns/siftsmall/siftsmall_query.fvecs_array_one_point",
+    128,
+  )
+  .unwrap();
+  search_range(query_vecs, base_vecs, 1000, 0, 25000, sq_euclidean);
+}
+*/
+
+trait PrimitiveToF32 {
+  fn tof32(self) -> f32;
+}
+
+impl PrimitiveToF32 for u8 {
+  fn tof32(self) -> f32 {
+    self as f32
+  }
+}
+
+impl PrimitiveToF32 for i32 {
+  fn tof32(self) -> f32 {
+    self as f32
+  }
+}
+
+impl PrimitiveToF32 for f32 {
+  fn tof32(self) -> f32 {
+    self
+  }
+}
+
+fn sq_euclidean_faster<T: PrimitiveToF32 + Copy>(v1: &[T], v2: &[T]) -> f32 {
   let mut result = 0.0;
   let n = v1.len();
   for i in 0..n {
-    let diff = v2[i].into() - v1[i].into();
+    let diff = v2[i].tof32() - v1[i].tof32();
     result += diff * diff;
   }
   result
+}
+
+fn sq_euclidean_slower<T: PrimitiveToF32 + Copy + Into<f32>>(v1: &[T], v2: &[T]) -> f32 {
+  let mut result = 0.0;
+  let n = v1.len();
+  for i in 0..n {
+    let diff: f32 = v2[i].into() - v1[i].into();
+    result += diff * diff;
+  }
+  result
+}
+
+//same performance as sq_euclidean_faster
+fn sq_euclidean_with_zip<T: PrimitiveToF32 + Copy + Into<f32>>(v1: &[T], v2: &[T]) -> f32 {
+  v1.iter()
+    .zip(v2.iter())
+    .map(|(x, y)| ((x.tof32()) - (y.tof32())) * ((x.tof32()) - (y.tof32())))
+    .fold(0.0, ::std::ops::Add::add)
 }
 
 fn main() {
   let base_vecs = Vecs::<u8>::new("/mnt/970pro/anns/bigann_base.bvecs_array", 128).unwrap();
   let query_vecs =
     Vecs::<u8>::new("/mnt/970pro/anns/bigann_query.bvecs_array_one_point", 128).unwrap();
-  search_range(query_vecs, base_vecs, 1000, 0, 999999999, sq_euclidean);
+  search_range(
+    query_vecs,
+    base_vecs,
+    1000,
+    0,
+    999999999,
+    sq_euclidean_faster,
+  );
 }
