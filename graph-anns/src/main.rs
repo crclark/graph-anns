f3d0217e9f6f3e82abdf4c761b75682a5ad17096
Connor Clark <connor.r.clark@gmail.com>
2022-01-02 15:01:09 -0500

    Make search_range more generic

--- graph-anns/src/main.rs
+++ graph-anns/src/main.rs
@@ -1,61 +1,10 @@
 #![feature(total_cmp)]
-extern crate memmap;
-extern crate nix;
-use nix::sys::mman::{mmap, MapFlags, ProtFlags};
 use std::cmp::Ordering;
 use std::collections::binary_heap::BinaryHeap;
-use std::convert::TryInto;
-use std::fs::File;
-use std::os::unix::io::IntoRawFd;
 use std::thread;
 use std::time::Instant;
 
-// A handle to a file in the bvecs_array, ivecs_array, or fvecs_array format on
-// disk. The file is mmaped. The associated functions are used to read vectors
-// from the file.
-#[derive(Debug, Clone, Copy)]
-struct Vecs<'a, T> {
-  pub num_rows: usize,
-  pub num_dim: usize,
-  buffer: &'a [T],
-}
-
-impl<'a, T> Vecs<'_, T> {
-  pub fn new<P>(path: P, num_dim: usize) -> Result<Self, String>
-  where
-    P: AsRef<std::path::Path>,
-  {
-    let f = File::open(path).expect("Failed to open file");
-    let filesize: usize = f
-      .metadata()
-      .expect("Failed to read file size")
-      .len()
-      .try_into()
-      .unwrap();
-    let num_rows = filesize / (num_dim * std::mem::size_of::<T>());
-    let buffer = unsafe {
-      let mmap = mmap(
-        std::ptr::null_mut(),
-        filesize,
-        ProtFlags::PROT_READ,
-        MapFlags::MAP_PRIVATE | MapFlags::MAP_POPULATE,
-        f.into_raw_fd(),
-        0,
-      )
-      .expect("mmap failed");
-      std::slice::from_raw_parts(mmap as *const T, num_rows * num_dim)
-    };
-    Ok(Self {
-      num_rows,
-      num_dim,
-      buffer,
-    })
-  }
-
-  pub fn get(&'a self, i: usize) -> &'a [T] {
-    &self.buffer[i * self.num_dim..i * self.num_dim + self.num_dim]
-  }
-}
+mod texmex;
 
 #[derive(Debug)]
 struct SearchResult {
@@ -89,21 +38,22 @@ impl Ord for SearchResult {
   }
 }
 
-fn search_range<'a, T>(
-  query_set: &Vecs<'a, T>,
-  db: &Vecs<'a, T>,
+fn search_range<'a, T: ?Sized, C: std::ops::Index<usize, Output = T>>(
+  query_set: &C,
+  query_set_size: usize,
+  db: &C,
   k: usize,
   lower_bound_incl: usize,
   upper_bound_excl: usize,
-  dist_fn: fn(&[T], &[T]) -> f32,
+  dist_fn: fn(&T, &T) -> f32,
 ) -> Vec<BinaryHeap<SearchResult>> {
   let mut nearest_neighbors = Vec::new();
-  for _ in 0..query_set.num_rows {
+  for _ in 0..query_set_size {
     nearest_neighbors.push(BinaryHeap::new());
   }
   for i in lower_bound_incl..upper_bound_excl {
-    for q in 0..query_set.num_rows {
-      let dist = dist_fn(db.get(i), query_set.get(q));
+    for q in 0..query_set_size {
+      let dist = dist_fn(&db[i], &query_set[q]);
       let heap: &mut BinaryHeap<SearchResult> = nearest_neighbors.get_mut(q).unwrap();
       heap.push(SearchResult::new(i, dist));
       while heap.len() > k {
@@ -114,43 +64,11 @@ fn search_range<'a, T>(
   nearest_neighbors
 }
 
-trait PrimitiveToF32 {
-  fn tof32(self) -> f32;
-}
-
-impl PrimitiveToF32 for u8 {
-  fn tof32(self) -> f32 {
-    self as f32
-  }
-}
-
-impl PrimitiveToF32 for i32 {
-  fn tof32(self) -> f32 {
-    self as f32
-  }
-}
-
-impl PrimitiveToF32 for f32 {
-  fn tof32(self) -> f32 {
-    self
-  }
-}
-
-fn sq_euclidean_faster<T: PrimitiveToF32 + Copy>(v1: &[T], v2: &[T]) -> f32 {
-  let mut result = 0.0;
-  let n = v1.len();
-  for i in 0..n {
-    let diff = v2[i].tof32() - v1[i].tof32();
-    result += diff * diff;
-  }
-  result
-}
-
 fn main() {
   let start = Instant::now();
-  let base_vecs = Vecs::<u8>::new("/mnt/970pro/anns/bigann_base.bvecs_array", 128).unwrap();
+  let base_vecs = texmex::Vecs::<u8>::new("/mnt/970pro/anns/bigann_base.bvecs_array", 128).unwrap();
   let query_vecs =
-    Vecs::<u8>::new("/mnt/970pro/anns/bigann_query.bvecs_array_one_point", 128).unwrap();
+    texmex::Vecs::<u8>::new("/mnt/970pro/anns/bigann_query.bvecs_array_one_point", 128).unwrap();
   println!("Loaded dataset in {:?}", start.elapsed());
 
   let mut handles = Vec::new();
@@ -171,13 +89,14 @@ fn main() {
       i, lower_bound, upper_bound
     );
     handles.push(thread::spawn(move || {
-      search_range(
+      search_range::<[u8], texmex::Vecs<u8>>(
         &query_vecs,
+        query_vecs.num_rows,
         &base_vecs,
         k,
         lower_bound_clone,
         upper_bound,
-        sq_euclidean_faster,
+        texmex::sq_euclidean_faster,
       );
     }));
     lower_bound += rows_per_thread;
@@ -187,3 +106,34 @@ fn main() {
     handle.join().unwrap();
   }
 }
+
+// test to make sure I understand how to share a vec of atomics between threads.
+
+// TODO: https://travisdowns.github.io/blog/2020/07/06/concurrency-costs.html
+// Read this a second time. It's great. See especially "Level 0" section.
+
+// use std::sync::atomic::AtomicU32;
+// use std::sync::Arc;
+// fn main() {
+//   let atomics = Arc::new(vec![AtomicU32::new(2), AtomicU32::new(1)]);
+
+//   let num_threads = 2;
+
+//   let mut handles = Vec::new();
+
+//   for i in 0..num_threads {
+//     let thread_num = i.clone();
+//     let thread_atomics = Arc::clone(&atomics);
+//     handles.push(thread::spawn(move || {
+//       let my_atomic = &thread_atomics[thread_num];
+//       let other_atomic = &thread_atomics[(thread_num + 1) % num_threads];
+//       let other_atomic_value = other_atomic.load(std::sync::atomic::Ordering::Relaxed);
+//       my_atomic.fetch_add(other_atomic_value, std::sync::atomic::Ordering::Relaxed)
+//     }));
+//   }
+
+//   for handle in handles {
+//     handle.join().unwrap();
+//   }
+//   println!("{:?}", atomics)
+// }
